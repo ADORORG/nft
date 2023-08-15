6ffd79b0ef667f5e0cf06e49d6d0a6dd99aec207
wisdomabioye <wisdomabioye@gmail.com>
2023-08-15 20:55:10 +0100

    Switch token page from parallel routing to normal

--- app/contract/[chainId]/[contractAddress]/[tokenId]/layout.tsx
+++ app/contract/[chainId]/[contractAddress]/[tokenId]/layout.tsx
@@ -2,16 +2,20 @@
 interface TokenPageProps {
     children: React.ReactNode,
     // market?: React.ReactNode,
-    token: React.ReactNode
+    // token: React.ReactNode
 }
 
-export default function Layout({children, token}: TokenPageProps) {
+/**
+ * @todo - Switch to parallel routing
+ * @param param0 
+ * @returns 
+ */
+export default function Layout({children}: TokenPageProps) {
 
     return (
         <div className="bg-white dark:bg-gray-950">
             <div className="container mx-auto py-6">
                 {children}
-                {token}
             </div>
         </div>
     )
