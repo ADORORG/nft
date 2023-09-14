a9c8e201d77943fda081693bf95df5a0f67dc481
wisdomabioye <wisdomabioye@gmail.com>
2023-09-14 22:30:29 +0100

    Updated Hero text

--- components/LandingPage/Hero/HeroText.tsx
+++ components/LandingPage/Hero/HeroText.tsx
@@ -5,11 +5,11 @@ export default function HeroText() {
         <div className="w-full lg:w-1/2">
             <div className="lg:max-w-lg">
                 <h1 className="text-4xl md:text-6xl lg:text-7xl tracking-wide subpixel-antialiased">
-                    Collect & Trade <br/>Unique Digital<br/>Assets 
+                    Create & Collect <br/>Unique Digital<br/>Items 
                 </h1>
                 
                 <p className="py-6 opacity-60 text-lg font-normal leading-8 tracking-wide subpixel-antialiased">
-                    Reputable NFT marketplace <br/>with thousands of curator, creator & artist.
+                    State of the art Marketplace <br/>Join us, as we build for the Global Creative Community.
                 </p>
 
                 <HeroButton />
