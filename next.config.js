cc65ff4dbe728483c93be286c87a13c24b9a8c0b
wisdomabioye <wisdomabioye@gmail.com>
2023-09-26 11:52:55 +0100

    Update: downgrade nextjs

--- next.config.js
+++ next.config.js
@@ -19,9 +19,6 @@ const nextConfig = {
         ]
     },
     reactStrictMode: true,
-    experimental: {
-        appDir: true,
-    },
     webpack: config => {
         config.resolve.fallback = { fs: false, net: false, tls: false };
         config.externals.push('pino-pretty', 'lokijs', 'encoding');
