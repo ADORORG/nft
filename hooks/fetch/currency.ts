b6994a9bb0b80b01d884e333fc55fbab267086cd
wisdomabioye <wisdomabioye@gmail.com>
2023-10-17 21:03:20 +0100

    update: fetch all currency refresh interval to 5 secs

--- hooks/fetch/currency.ts
+++ hooks/fetch/currency.ts
@@ -1,11 +1,14 @@
 import type { CryptocurrencyType } from "@/lib/types/currency"
 import type { AppRouterApiResponseType } from "@/lib/types/common"
 import { replaceUrlParams } from "@/utils/main"
+import { fetcher } from "@/utils/network"
 import useSWR from "swr"
 import apiRoutes from "@/config/api.route"
 
 export function useAllCurrencies() {
-    const { data, error, isLoading } = useSWR<AppRouterApiResponseType<CryptocurrencyType[]>>(apiRoutes.getAllCurrency)
+    const { data, error, isLoading } = useSWR<AppRouterApiResponseType<CryptocurrencyType[]>>(apiRoutes.getAllCurrency, fetcher, {
+        refreshInterval: 5000
+    })
 
     return {
         currencies: data ? data.data : undefined,
@@ -18,7 +21,11 @@ export function useChainCurrencies(chainId: string) {
     const { data, error, isLoading } = useSWR<AppRouterApiResponseType<CryptocurrencyType[]>>(
         chainId ? 
         replaceUrlParams(apiRoutes.getCurrenciesByChain, {chainId}) 
-        : null
+        : null,
+        fetcher,
+        {
+            refreshInterval: 5000
+        }
     )
 
     return {
