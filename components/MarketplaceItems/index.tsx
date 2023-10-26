87d6413085053b8081d4cf00e05347e4b92607e6
wisdomabioye <wisdomabioye@gmail.com>
2023-10-26 11:51:06 +0100

    Update: marketplace order display enhanced

--- components/MarketplaceItems/index.tsx
+++ components/MarketplaceItems/index.tsx
@@ -21,6 +21,7 @@ export default function MarketplaceItems(props: MarketplaceItems) {
         <div className="flex flex-col gap-4 py-8 px-4">
             <div className="flex flex-row flex-wrap items-center justify-center lg:justify-start gap-4">
                 {
+                    props.orders && props.orders.length > 0 ?
                     props.orders
                     // Not filter by nftSchema or nftSchema === "all" otherwise, filter nftSchema
                     .filter(o => (
@@ -45,6 +46,10 @@ export default function MarketplaceItems(props: MarketplaceItems) {
                             marketOrder={order}
                         />
                     ))
+                    :
+                    <div className="px-2 py-6">
+                        <p className="">No orders found</p>
+                    </div>
                 }
             </div>
             <Pagination 
